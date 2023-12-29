96f94cf408bc5ed322718e16083bc82d25ede57e
Vid Luther <vid@luther.io>
2023-12-28 22:17:05 -0600

    Initial import

---
+++ README.md
@@ -0,0 +1,34 @@
+# Content Security Policynator...
+
+## Overview
+
+The CSP Directive Editor is a Node.js tool designed to simplify the process of managing Content Security Policies (CSP). It addresses the challenge of editing and understanding complex CSP strings, which are often difficult to handle due to their length and lack of structure when presented as a single line of text. This tool allows users to fetch CSP from a given URL, displays the current directives in an organized and readable format, and provides an interactive command-line interface to edit these directives or add new ones.
+
+## Problem Statement
+
+Content Security Policies are a critical part of web application security, helping to prevent various types of attacks like Cross-Site Scripting (XSS) and data injection. However, managing and editing CSPs can be cumbersome, especially when dealing with long and complex policies. This tool aims to make CSP management more accessible and manageable by providing an interactive way to edit CSP directives, view changes in real-time, and understand the structure of a CSP at a glance.
+
+## Features
+
+- Fetch CSP headers directly from URLs.
+- Deserialize CSP headers into individual directives for easy reading and editing.
+- Interactive command-line interface to edit existing directives or add new ones.
+- Serialize edited directives back into a cohesive CSP string.
+- Simple and intuitive user interaction model.
+
+## Getting Started
+
+### Prerequisites
+
+- Node.js (version 12.x or higher)
+- Internet access for fetching CSP from URLs
+
+### Quick Start
+
+```bash
+   git clone https://github.com/[YourUsername]/csp-directive-editor.git
+   cd csp-directive-editor
+   npm install
+
+   node getcsp.js https://example.com 
+```
---
+++ getcsp.js
@@ -0,0 +1,117 @@
+import fetch from "node-fetch";
+import readline from "readline";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+async function getCSP(url) {
+  try {
+    const response = await fetch(url);
+    const csp = response.headers.get("content-security-policy");
+
+    if (csp) {
+      return deserializeCSP(csp);
+    } else {
+      console.log("No Content Security Policy found for", url);
+      return {};
+    }
+  } catch (error) {
+    console.error("Error fetching the URL:", error.message);
+    return {};
+  }
+}
+
+function deserializeCSP(cspString) {
+  const directives = {};
+  const pairs = cspString
+    .split(";")
+    .map((d) => d.trim())
+    .filter((d) => d);
+
+  pairs.forEach((pair) => {
+    const [directive, ...values] = pair.split(" ").map((v) => v.trim());
+    directives[directive] = values;
+  });
+
+  return directives;
+}
+
+function serializeCSP(directives) {
+  return Object.entries(directives)
+    .map(([key, values]) => `${key} ${values.join(" ")}`)
+    .join("; ");
+}
+
+function editCSP(directives) {
+  console.log(
+    "\x1b[36m%s\x1b[0m", // Cyan color for headings
+    `Your CSP has ${
+      Object.keys(directives).length
+    } directives. They are as follows:`
+  );
+  Object.entries(directives).forEach(([directive, values], index) => {
+    console.log(
+      `${index + 1}: \x1b[33m${directive}\x1b[0m - ${values.join(" ")}`
+    ); // Yellow for directive names
+  });
+  console.log("\x1b[32m%s\x1b[0m", "A: Add a new directive");
+  console.log("\x1b[32m%s\x1b[0m", "P: Print the full CSP string and exit");
+
+  rl.question(
+    "Select a directive number to edit, A to add, or P to print: ",
+    (answer) => {
+      if (answer.toUpperCase() === "P") {
+        console.log("Final CSP String:", serializeCSP(directives));
+        rl.close();
+        return;
+      } else if (answer.toUpperCase() === "A") {
+        addDirective(directives);
+        return;
+      }
+
+      const directiveIndex = parseInt(answer) - 1;
+      const directiveKeys = Object.keys(directives);
+      if (directiveIndex >= 0 && directiveIndex < directiveKeys.length) {
+        const directive = directiveKeys[directiveIndex];
+        rl.question(
+          `Edit the value for \x1b[33m${directive}\x1b[0m (current: ${directives[
+            directive
+          ].join(" ")}): `,
+          (newValue) => {
+            directives[directive] = newValue.split(" ").filter((v) => v);
+            editCSP(directives);
+          }
+        );
+      } else {
+        console.log("Invalid selection. Please try again.");
+        editCSP(directives);
+      }
+    }
+  );
+}
+
+function addDirective(directives) {
+  rl.question(
+    "Enter the new directive name and values (e.g., 'script-src https://example.com'): ",
+    (newDirective) => {
+      const [directive, ...values] = newDirective.split(" ").filter((v) => v);
+      if (directive) {
+        directives[directive] = values;
+        editCSP(directives);
+      } else {
+        console.log("Invalid directive. Please try again.");
+        addDirective(directives);
+      }
+    }
+  );
+}
+
+const url = process.argv[2];
+if (!url) {
+  console.log("Please provide a URL as an argument");
+  rl.close();
+} else {
+  getCSP(url).then((directives) => editCSP(directives));
+}
